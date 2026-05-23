51950c05fd8eb1a0dcd3dcaf4c9256303cab5b1b
Vladimir <donskov.vd@gmail.com>
2026-05-23 19:08:47 +0300

    Use Telegram storage for daily reward claims

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cypclick-v11'; // bumped version - clears old cache for all users
+const CACHE_NAME = 'cypclick-v13'; // bumped version - clears old cache for all users
 const STATIC_ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
