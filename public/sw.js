33a3c18cb192ad92a5a3286fc132231562ef9d60
Vladimir <donskov.vd@gmail.com>
2026-05-23 19:33:20 +0300

    Restore bot webapp URL

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cypclick-v15'; // bumped version - clears old cache for all users
+const CACHE_NAME = 'cypclick-v16'; // bumped version - clears old cache for all users
 const STATIC_ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
